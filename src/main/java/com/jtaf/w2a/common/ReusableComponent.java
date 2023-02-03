5433dd7d0fe063e9b53105653dbf4fda0d8d88a9
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-02-03 14:27:08 +0530

    ScreenCapture Utility added and linked to Listener

--- src/main/java/com/jtaf/w2a/common/ReusableComponent.java
+++ src/main/java/com/jtaf/w2a/common/ReusableComponent.java
@@ -1,7 +1,15 @@
 package com.jtaf.w2a.common;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import com.jtaf.w2a.base.TestBase;
 
@@ -16,4 +24,21 @@ public class ReusableComponent extends TestBase {
 			return false;
 		}
 	}
+
+	public static String captureSnapShot() {
+
+		Calendar calendar = Calendar.getInstance();
+		// System.out.println(cal.get(Calendar.DATE)+"_"+(cal.get(Calendar.MONTH)+1)+"_"+cal.get(Calendar.YEAR));
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd_MM_YYYY_hh_mm_ss");
+		File source = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		File destination = new File(System.getProperty("user.dir") + "/src/test/resources/screenshots/success/"
+				+ simpleDateFormat.format(calendar.getTime()) + ".png");
+		try {
+			FileUtils.copyFile(source, destination);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return destination.getAbsolutePath();
+	}
+
 }
--- src/main/java/com/jtaf/w2a/listeners/ReportListener.java
+++ src/main/java/com/jtaf/w2a/listeners/ReportListener.java
@@ -1,21 +1,13 @@
 package com.jtaf.w2a.listeners;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.Reporter;
 
-import com.jtaf.w2a.base.TestBase;
+import com.jtaf.w2a.common.ReusableComponent;
 
-public class ReportListener extends TestBase implements ITestListener {
+public class ReportListener extends ReusableComponent implements ITestListener {
 
 	public void onTestStart(ITestResult result) {
 		// not implemented
@@ -24,21 +16,11 @@ public class ReportListener extends TestBase implements ITestListener {
 	public void onTestSuccess(ITestResult result) {
 
 		System.setProperty("org.uncommons.reportng.escape-output", "false");
-		Calendar calender = Calendar.getInstance();
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd_MM_YYYY_hh_mm_ss");
-		File source = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		File destination = new File(System.getProperty("user.dir") + "/src/test/resources/screenshots/success/"
-				+ result.getName() + "_" + simpleDateFormat.format(calender.getTime()) + ".png");
-		try {
-			FileUtils.copyFile(source, destination);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		Reporter.log("<br>");
 		Reporter.log(result.getMethod().getMethodName() + " Test Passed..!!");
 		Reporter.log("<br>");
-		Reporter.log("<a target='_blank' href='" + destination.getAbsolutePath() + "'><img src='"
-				+ destination.getAbsolutePath() + "' height='100' width='100'/></a>");
+		Reporter.log("<a target='_blank' href='" + captureSnapShot() + "'><img src='" + captureSnapShot()
+				+ "' height='100' width='100'/></a>");
 	}
 
 	public void onTestFailure(ITestResult result) {
